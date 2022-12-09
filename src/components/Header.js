3124ba38af933ecbd9a96504ad86c0faf1d0dff7
monie-B <mbullock091@icloud.com>
2022-12-09 14:53:29 -0500

    made header logo and name link to homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,13 @@ const Header = () => {
     <>
       <div>
         <header className='header'>
-            <img src='IMG_4260.jpeg' alt='Moni B Artistry logo' className='logo'></img>
+            <a href='/'>
+              <img src='IMG_4260.jpeg' alt='Moni B Artistry logo' className='logo'></img>
+            </a>
             <h1 className='home-title'>
-                Moni B Artistry
+                <a href='/' className='mbalink'>
+                  Moni B Artistry
+                </a>
             </h1>
         </header>
       </div>
