fea37530d12b9d5cc84c965923338eaa7d4759aa
monie-B <mbullock091@icloud.com>
2022-12-15 22:09:16 -0500

    working on sip and paint page

--- src/pages/SipAndPaint.js
+++ src/pages/SipAndPaint.js
@@ -9,6 +9,24 @@ const SipAndPaint = () => {
     <>
       <Header />
       <NavBar />
+      <section>
+        <h1 className='s-title'>
+          Host a Sip & Paint With Me
+        </h1>
+      </section>
+      <div className='sectionOne'>
+        <h3>
+          Plan in Advance or Last Minute
+        </h3>
+        <div>
+          <p>
+            Paint & Sip parties are great for a relaxed and yet entertaining party. You'll soon learn the calming effects of painting, while still having a great time with your family and friends. 
+          </p>
+        </div>
+      </div>
+      <div className='art1'>
+        <img src='IMG_4391.JPG' alt='art' className='art'/>
+      </div>
     </>
   )
 }
