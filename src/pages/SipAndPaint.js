e5506fd66695770d3caf382f463eb3cf6d3de001
monie-B <mbullock091@icloud.com>
2022-12-18 23:46:13 -0500

    continue working on sip and paint page

--- src/pages/SipAndPaint.js
+++ src/pages/SipAndPaint.js
@@ -25,7 +25,20 @@ const SipAndPaint = () => {
         </div>
       </div>
       <div className='art1'>
-        <img src='IMG_4391.JPG' alt='art' className='art'/>
+        <img src='firstSP.JPEG' alt='art' className='art'/>
+      </div>
+      <div className='containerTwo'>
+        <div className='art2'>
+          <img src='SecondSP.jpeg' alt='Sip and Paint event' className='artpictwo'/>
+        </div>
+        <div className='textContainer'>
+          <h3>
+            Gather With Family & Friends
+          </h3>
+            <p>
+              Paint & Sip parties are great for a relaxed and yet entertaining party. You'll soon learn the calming effects of painting, while still having a great time with your family and friends. 
+            </p>
+        </div>
       </div>
     </>
   )
