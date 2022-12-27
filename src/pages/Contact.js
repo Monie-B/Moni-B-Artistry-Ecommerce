8b3920f0154dd4acbe88c16da2b87d03f86f8beb
monie-B <mbullock091@icloud.com>
2022-12-27 15:13:25 -0500

    stylized form on Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,13 +13,13 @@ const Contact = () => {
           Got a Question? Contact Me!
         </section>
         <section>
-        <form action="mailto:someone@example.com" method="post" enctype="text/plain">
+        <form action="mailto:monibartistry@gmail.com" method="post" enctype="text/plain" className='formText'>
           Name:<br/>
-          <input type="text" name="name"/><br/>
+          <input type="text" name="name" className='nameInput'/><br/>
           E-mail:<br/>
-          <input type="text" name="mail"/><br/>
-          Comment:<br/>
-          <textarea type="text" name="comment" size="50"></textarea><br/><br/>
+          <input type="text" name="mail" className='emailInput'/><br/>
+          Message:<br/>
+          <textarea type="text" name="comment" size="50" className='messageInput'></textarea><br/>
           <input type="submit" value="Send"/>
           <input type="reset" value="Reset"/>
         </form>
