9df35756e9456c0034e16be13950b845e1c04c5a
monie-B <mbullock091@icloud.com>
2022-12-30 08:39:42 -0500

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/footer.css'
+
+const Footer = () => {
+  return (
+    <>
+        <div>
+            <p className='footer'>This is the footer</p>
+        </div>
+    </>
+  )
+}
+
+export default Footer
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../css/contact.css';
 import Header from '../components/Header';
 import NavBar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 
 const Contact = () => {
@@ -15,15 +16,16 @@ const Contact = () => {
         <section>
         <form action="mailto:monibartistry@gmail.com" method="post" enctype="text/plain" className='formText'>
           Name:<br/>
-          <input type="text" name="name" className='nameInput'/><br/>
+          <input type="text" name="name" className='nameInput' placeholder='Full Name' required /><br/>
           E-mail:<br/>
-          <input type="text" name="mail" className='emailInput'/><br/>
+          <input type="text" name="mail" className='emailInput' placeholder='Email Address' required /><br/>
           Message:<br/>
-          <textarea type="text" name="comment" size="50" className='messageInput'></textarea><br/>
-          <input type="submit" value="Send"/>
-          <input type="reset" value="Reset"/>
+          <textarea type="text" name="comment" size="50" className='messageInput' placeholder='How Can I Help You?' required ></textarea><br/>
+          <input type="submit" value="Send" className='sendBtn'/>
+          <input type="reset" value="Reset" className='resetBtn'/>
         </form>
         </section>
+        <Footer />
     </>
   )
 }
