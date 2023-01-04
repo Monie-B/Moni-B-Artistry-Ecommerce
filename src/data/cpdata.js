73b7297aebb8f8c6b2533c0a8f51ce8e3900577d
monie-B <mbullock091@icloud.com>
2023-01-03 21:29:47 -0500

    created data files

---
+++ src/data/cpdata.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        img: 'BeachBabes.JPEG',
+        title: 'Beach Babes',
+        price: '$51.52',
+        inStock: true
+    },
+    {
+        img: 'Butterflies.jpeg',
+        title: 'You Give Me Butterflies',
+        price: 150,
+        inStock: true
+    },
+    {
+        img: 'YouWooMe.jpg',
+        title: 'You Woo Me',
+        price: 150,
+        inStock: true
+    },
+]
+
+
+// {
+//     img: '',
+//     title: '',
+//     price: '',
+//     inStock: true
+// },
