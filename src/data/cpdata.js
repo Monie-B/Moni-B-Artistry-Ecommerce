05d129fb83afa4bec002d8561ff0fdf304a9de8a
monie-B <mbullock091@icloud.com>
2023-01-04 07:41:24 -0500

    completed cp data

--- src/data/cpdata.js
+++ src/data/cpdata.js
@@ -12,6 +12,55 @@ export default [
         inStock: true
     },
     {
+        img: 'Daisy.jpg',
+        title: 'Daisy',
+        price: [
+            51.75,
+            62.04,
+            72.33,
+            82.62,
+            92.91
+        ],
+        inStock: true,
+        size: [
+            '15x11',
+            '22x16',
+            '24x18',
+            '36x27',
+            '40x30'
+        ]
+    },
+    {
+        img: 'FreeSpirit.jpg',
+        title: 'Free Spirit',
+        price: '51.75',
+        inStock: true
+    },
+    {
+        img: 'HumanNature.jpg',
+        title: 'Human Nature',
+        price: '51.75',
+        inStock: true
+    },
+    {
+        img: 'MelaninUniverse.jpg',
+        title: 'Melanin Universe',
+        price: '51.75',
+        inStock: true
+    },
+    {
+        img: 'Motherland.jpg',
+        title: 'Motherland',
+        price: '51.75',
+        inStock: true
+    },
+    {
+        img: 'Purebeauty.jpg',
+        title: 'Pure Beauty',
+        price: '51.75',
+        inStock: true
+    },
+    {
         img: 'YouWooMe.jpg',
         title: 'You Woo Me',
         price: 150,
